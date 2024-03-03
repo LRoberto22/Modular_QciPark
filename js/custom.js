b88b9ab1954da45216015832a3e8b7f838228fc4
Luis Fernando Mora <111839033+Omegataker3@users.noreply.github.com>
2024-03-03 00:34:06 -0600

    Pa calarle

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,22 @@
+$(document).ready(function() {
 
+    // Function to get the current year and display it
+    function getYear() {
+        var currentDate = new Date();
+        var currentYear = currentDate.getFullYear();
+        document.querySelector('#displayYear').innerHTML = currentYear;
+    }
 
-// Function to get the current year and display it
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector('#displayYear').innerHTML = currentYear;
-}
-
+    function obtenerPregunta() {
+        $.get('http://0.0.0.0:8000/obtener-usuario-horario', function(data) {
+            console.log("Desayuna con huevo");
+            if (data) {
+                salida = data.salida;
+                console.log(salida);
+                $('.Salida').text("Quisiera ser una mosca");
+            } else {
+                console.log("No jalo");  
+            }
+        });
+    }
+});
